9419911d791da6400f7934fa01144bd8e6da083b
Bhaikko <siddhartha.pawar17@gmail.com>
2019-06-28 21:12:36 +0530

    IDs assigned

---
+++ Calculator/script.js
@@ -0,0 +1,8 @@
+var outputString = "";
+var expressionString = "";
+
+let outputBox = document.getElementById("outputBox");
+let expression = document.getElementById("expression");
+let output = document.getElementById("output");
+
+
